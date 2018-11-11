910eebd6b151fc16902fe4f773eab58b063e1388
T00064-RD3(林卓志) <T00064@senao.com.tw>
2018-11-11 11:30:22 +0800

    route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { HttpClientModule } from "@angular/common/http";
-import { AppComponent } from "./app.component";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Route } from '@angular/router';
+import { AppComponent } from './app.component';
+import { Page1Component } from './page1/page1.component';
+
+const routes: Route[] = [
+  { path: 'page1', component: Page1Component },
+  { path: '**', redirectTo: '/page1', pathMatch: 'full' }
+];
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [AppComponent, Page1Component],
+  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent]
 })
