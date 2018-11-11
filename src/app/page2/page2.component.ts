808dc13423710a61894ef554174ab354bf510379
T00064-RD3(林卓志) <T00064@senao.com.tw>
2018-11-11 11:53:41 +0800

    link other page button

--- src/app/page2/page2.component.ts
+++ src/app/page2/page2.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page2',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Page2Component implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  gotoPage1()
+  {
+    this.router.navigate(['/page1']);
   }
 
 }
