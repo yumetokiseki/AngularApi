59c4c82a1fdde847411df19975670d345036d626
T00064-RD3(林卓志) <T00064@senao.com.tw>
2018-11-11 11:34:06 +0800

    muti page router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Route } from '@angular/router';
 import { AppComponent } from './app.component';
 import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
 
 const routes: Route[] = [
   { path: 'page1', component: Page1Component },
+  { path: 'page2', component: Page2Component },
   { path: '**', redirectTo: '/page1', pathMatch: 'full' }
 ];
 
 @NgModule({
-  declarations: [AppComponent, Page1Component],
+  declarations: [AppComponent, Page1Component, Page2Component],
   imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent]
