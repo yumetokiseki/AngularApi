2f3732ccb1235c6d51b11e0ed2eca96a56b63ead
T00064-RD3(林卓志) <T00064@senao.com.tw>
2018-11-11 11:02:30 +0800

    api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { map } from "rxjs/operators";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'api';
+  title = "api";
+
+  posts = [];
+
+  apisource = this.http.get("https://jsonplaceholder.typicode.com/posts").pipe(
+    map((posts: any[]) => {
+      return posts.map((post: any) => {
+        return { id: post.id, title: post.title };
+      });
+    })
+  );
+
+  constructor(private http: HttpClient) {}
+
+  loadData() {
+    this.apisource.subscribe(data => {
+      this.posts = data;
+    });
+  }
 }
